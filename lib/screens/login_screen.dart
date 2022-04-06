92fd6b7f1474d56dc37f1f7041d92bfc6a1fbeb8
cloudfusion466 <cloudfusion466@gmail.com>
2022-04-06 11:36:05 -0300

    Inicio do repossitório

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_clone/widgets/custom_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'iniciar ou participar de uma reunião',
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 38.0),
+            child: Image.asset('assets/image/onboarding.jpg'),
+          ),
+          CustomButton(
+            text: 'Login do Google',
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
